736932296fde3ccdbcfb365580354b2a0fc20611
marialungu <anauna.lungu@gmail.com>
2018-05-04 13:29:26 +0300

    DBTRAIN-63 - create new component for admin CREATE; create admin dashboard UI (work in progress)

--- dbTrainingEnroll/src/app/admin/admin.module.ts
+++ dbTrainingEnroll/src/app/admin/admin.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin.component';
+import {MaterialModule} from "../material.module";
+import {MatTabsModule} from "@angular/material";
+import { AddTrainingsComponent } from './add-trainings/add-trainings.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    MatTabsModule
   ],
-  declarations: [AdminComponent]
+  declarations: [AdminComponent, AddTrainingsComponent]
 })
 export class AdminModule { }
