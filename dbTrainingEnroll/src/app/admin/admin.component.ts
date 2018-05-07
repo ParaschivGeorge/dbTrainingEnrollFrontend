8543ea7758cc485ca9748cc17476410ffea4a23d
Stefaneva <stefaneva25@yahoo.com>
2018-05-07 10:42:58 +0300

    Admin - excel

--- dbTrainingEnroll/src/app/admin/admin.component.ts
+++ dbTrainingEnroll/src/app/admin/admin.component.ts
@@ -8,11 +8,12 @@ import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
-  
+
   arrayBuffer:any;
   fileToUpload:File;
-  file: File = null;
-  adminAddForm: FormGroup;  
+  // file: File = null;
+  file:File;
+  adminAddForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder) { }
 
@@ -22,7 +23,7 @@ export class AdminComponent implements OnInit {
     });
   }
 
-  handleFileInput(files: FileList) {
+  // handleFileInput(files: FileList) {
     // let reader = new FileReader();
     // if(files && files.length > 0) {
     //   this.file = files[0];
@@ -34,31 +35,49 @@ export class AdminComponent implements OnInit {
     //   };
     // }
 
-    if(files && files.length > 0) {
-      this.file = files[0];
-      let fileReader = new FileReader();
+    // if(files && files.length > 0) {
+    //   this.file = files[0];
+    //   let fileReader = new FileReader();
+    //
+    //   fileReader.onload = (e) => {
+    //
+    //     this.arrayBuffer = fileReader.result;
+    //     var data = new Uint8Array(this.arrayBuffer);
+    //     var arr = new Array();
+    //     for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+    //     var bstr = arr.join("");
+    //     var workbook = XLSX.read(bstr, {type:"binary"});
+    //     var first_sheet_name = workbook.SheetNames[0];
+    //     var worksheet = workbook.Sheets[first_sheet_name];
+    //     this.adminAddForm.get('excelInput').setValue(this.file);;
+    //
+    //     console.log(XLSX.utils.sheet_to_json(worksheet,{raw:true}));
+    // }
+    //
+    // fileReader.readAsArrayBuffer(this.file);
+    //
+  // }
+  // }
 
-      fileReader.onload = (e) => {
-        
-        this.arrayBuffer = fileReader.result;
-        var data = new Uint8Array(this.arrayBuffer);
-        var arr = new Array();
-        for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
-        var bstr = arr.join("");
-        var workbook = XLSX.read(bstr, {type:"binary"});
-        var first_sheet_name = workbook.SheetNames[0];
-        var worksheet = workbook.Sheets[first_sheet_name];
-        this.adminAddForm.get('excelInput').setValue(this.file);;
-        
-        console.log(XLSX.utils.sheet_to_json(worksheet,{raw:true}));          
-    }
-    
-    fileReader.readAsArrayBuffer(this.file);
-        
+  incomingfile(event)
+  {
+    this.file= event.target.files[0];
   }
-}
-  
+
   onSubmit() {
-    console.log(this.adminAddForm);
+    // console.log(this.adminAddForm);
+    let fileReader = new FileReader();
+    fileReader.onload = (e) => {
+      this.arrayBuffer = fileReader.result;
+      const data = new Uint8Array(this.arrayBuffer);
+      const arr = [];
+      for(let i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+      const bstr = arr.join("");
+      const workbook = XLSX.read(bstr, {type: "binary"});
+      const first_sheet_name = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[first_sheet_name];
+      console.log(XLSX.utils.sheet_to_json(worksheet,{raw:true}));
+    };
+    fileReader.readAsArrayBuffer(this.file);
   }
 }
