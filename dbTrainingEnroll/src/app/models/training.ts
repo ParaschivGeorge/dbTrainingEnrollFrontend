48ecee84ed7bddbd80cf393714082be2a027f99b
Andrei Lazar <l.andrei37@yahoo.ro>
2018-05-09 16:35:39 +0300

    Removed Specs, Changed Duration into Vendor

--- dbTrainingEnroll/src/app/models/training.ts
+++ dbTrainingEnroll/src/app/models/training.ts
@@ -4,7 +4,6 @@ export interface Training {
     id: number;
     name: string;
     date: string;
-    duration: string;
     technology: string;
     categoryType: string;
     acceptedUsers: number;
